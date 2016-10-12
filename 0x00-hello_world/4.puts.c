4c65d27417d590581a4115972624ff129905e635
Corbin <corbin.coleman@holbertonschool.com>
2016-10-12 19:57:01 +0000

    Task 4

--- 0x00-hello_world/4.puts.c
+++ 0x00-hello_world/4.puts.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 
+/*Run the main function, which will print a statement*/
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
-  return 0;
+  return (0);
 }
