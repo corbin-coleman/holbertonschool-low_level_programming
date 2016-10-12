a3f3b7242d1884198c683b5cf7c93306d1a471b6
Corbin <corbin.coleman@holbertonschool.com>
2016-10-12 19:12:39 +0000

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(void)
+{
+  int i;
+  char c;
+  long int l;
+  long long int ll;
+  float f;
+
+  printf("The size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+  printf("The size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+  printf("The size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l));
+  printf("The size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(ll));
+  printf("The size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+
+  return 0;
+}
