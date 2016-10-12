d22e16c7cc713ffbf19812e2a30af59e4eaaf827
Corbin <corbin.coleman@holbertonschool.com>
2016-10-12 20:19:52 +0000

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,25 @@
 #include<stdio.h>
 
+/**
+ * main - Run main function
+ *
+ * Description: Run printf multiple times to print size of different types
+ * Return: Return 0 to exit function
+ */
 int main(void)
 {
-  int i;
-  char c;
-  long int l;
-  long long int ll;
-  float f;
+	int i;
+	char c;
+	long int l;
+	long long int ll;
+	float f;
 
-  printf("The size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
-  printf("The size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
-  printf("The size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l));
-  printf("The size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(ll));
-  printf("The size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	printf("The size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("The size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("The size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l));
+	printf("The size of a long long int: %lu byte(s)\n",
+	       (unsigned long)sizeof(ll));
+	printf("The size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 
-  return 0;
+	return (0);
 }
