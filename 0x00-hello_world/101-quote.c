f799a06b240b61fc0c41d04f2a5ce893ebfe1a0a
Corbin <corbin.coleman@holbertonschool.com>
2016-10-12 22:16:09 +0000

    Task 9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,7 +12,7 @@ int main(void)
 	int l;
 
 	l =
-	  sizeof("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+	  sizeof("and that piece of art is useful - Dora Korpar, 2015-10-19");
 
 	write(2,
 	      "and that piece of art is useful - Dora Korpar, 2015-10-19\n", l);
