564959c4f9043a0491eb313508645865d08eeaeb
Corbin <corbin.coleman@holbertonschool.com>
2016-10-13 21:14:40 +0000

    Task 11

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+/**
+ * main - print combos without repeats
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int f = '0';
+	int s = '0';
+
+	while (f <= '9')
+	{
+		s = '0';
+		while (s <= '9')
+		{
+			if (s == f)
+			{
+			}
+			else if (f > s)
+			{
+			}
+			else
+			{
+				putchar(f);
+				putchar(s);
+				if (s == '9' && f == '9')
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			s++;
+		}
+		f++;
+	}
+	putchar('\n');
+	return (0);
+}
