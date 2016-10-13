754d438dda3204105dc9680a37ed79c15f719397
Corbin <corbin.coleman@holbertonschool.com>
2016-10-13 17:20:25 +0000

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - print numbers 0-9
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		printf("%d", n);
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
