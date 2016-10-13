d7fbb404fa4dfff49a9988fc2bc8826ea8684290
Corbin <corbin.coleman@holbertonschool.com>
2016-10-13 20:37:07 +0000

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - print the alphabet in reverse
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char z = 'z';
+
+	while (z >= 'a')
+	{
+		putchar(z);
+		z--;
+	}
+	putchar('\n');
+	return (0);
+}
