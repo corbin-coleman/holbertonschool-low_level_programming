62fb3bfabb6338a3219506b46848d80c339285cd
Corbin <corbin.coleman@holbertonschool.com>
2016-10-17 22:12:38 +0000

    Task 11 - Print from a number to 98

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -8,9 +8,10 @@
  */
 int main(void)
 {
-	int n;
-
-	n = add(89, 9);
-	printf("%d\n", n);
+	print_to_98(0);
+	print_to_98(98);
+	print_to_98(111);
+	print_to_98(81);
+	print_to_98(-10);
 	return (0);
 }
