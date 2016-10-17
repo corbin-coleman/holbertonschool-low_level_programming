18502365b7d7445620718d09e8449939f591e58f
Corbin <corbin.coleman@holbertonschool.com>
2016-10-17 20:12:57 +0000

    Task 5 - Check sign of number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_sign - find if input is pos, neg, or 0
+ *
+ * Return: return 1 if pos, return 0 if 0, return -1 if neg
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return 1;
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return -1;
+	}
+	else
+	{
+		_putchar('0');
+		return 0;
+	}
+}
