4fbe385f00a52045837c5935e6c2e23927c4f627
Corbin <corbin.coleman@holbertonschool.com>
2016-10-17 20:54:02 +0000

    Task 6 - Absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+/**
+ * _abs - Convert to absolute value of an integer
+ * @n - the integer to affect
+ *
+ * Return: Return absolute value of integer
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (n * -1);
+	else
+		return n;
+}
