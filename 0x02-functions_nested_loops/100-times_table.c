9f216fe2537eb1ed12543ef158b66d764c627115
Corbin <corbin.coleman@holbertonschool.com>
2016-10-17 22:54:06 +0000

    Task 12 - Print any given times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,50 @@
+#include "holberton.h"
+
+/**
+ * print_times_table - Print the n times table, starting from 0
+ * @n: The times table to print
+ *
+ * Return: Nothing
+ */
+void print_times_table(int n)
+{
+	int x, y, mult, one, ten, hund;
+
+	x = 0;
+	if (n >= 0 && n <= 15)
+	{
+		while (x <= n)
+		{
+			y = 0;
+			while (y <= n)
+			{
+				mult = x * y;
+				one = mult % 10;
+				ten = mult % 100 / 10;
+				hund = mult / 100;
+				if (hund == 0)
+				{
+					_putchar(' ');
+					if (ten == 0)
+						_putchar(' ');
+					else
+						_putchar(ten + '0');
+				}
+				else
+				{
+					_putchar(hund + '0');
+					_putchar(ten + '0');
+				}
+				_putchar(one + '0');
+				if (y != n)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				y++;
+			}
+			_putchar('\n');
+			x++;
+		}
+	}
+}
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -8,10 +8,12 @@
  */
 int main(void)
 {
-	print_to_98(0);
-	print_to_98(98);
-	print_to_98(111);
-	print_to_98(81);
-	print_to_98(-10);
+	print_times_table(3);
+	_putchar('\n');
+	print_times_table(5);
+	_putchar('\n');
+	print_times_table(98);
+	_putchar('\n');
+	print_times_table(12);
 	return (0);
 }
