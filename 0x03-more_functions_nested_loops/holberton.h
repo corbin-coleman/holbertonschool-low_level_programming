f6631c49860ee11cd38b52d9e43884980dccd788
Corbin <corbin.coleman@holbertonschool.com>
2016-10-20 17:27:30 +0000

    Task 8 - Print a nxn square of #

--- 0x03-more_functions_nested_loops/holberton.h
+++ 0x03-more_functions_nested_loops/holberton.h
@@ -19,3 +19,4 @@ void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int);
 void print_diagonal(int);
+void print_square(int);
--- 0x03-more_functions_nested_loops/main.c
+++ 0x03-more_functions_nested_loops/main.c
@@ -8,9 +8,8 @@
  */
 int main(void)
 {
-	print_diagonal(0);
-	print_diagonal(2);
-	print_diagonal(10);
-	print_diagonal(-4);
+	print_square(2);
+	print_square(10);
+	print_square(0);
 	return (0);
 }
