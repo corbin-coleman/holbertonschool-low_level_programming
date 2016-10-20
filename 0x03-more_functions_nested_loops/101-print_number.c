a047f9eaed19f69988c9ab08251d7bcd9a802696
Corbin <corbin.coleman@holbertonschool.com>
2016-10-20 22:19:03 +0000

    Task 12 - Print integers using only  _putchar()

---
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,49 @@
+#include "holberton.h"
+
+/**
+ * print_number - Print any number one character at a time
+ * @n: Number to print
+ *
+ * Return: Nothing
+ */
+void print_number(int n)
+{
+	int hn;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= - 1;
+	}
+	if (n / 100000 != 0)
+	{
+		hn = n / 100000;
+		_putchar(hn / 10000 % 10 + '0');
+		_putchar(hn / 1000 % 10 + '0');
+		_putchar(hn / 100 % 10 + '0');
+		_putchar(hn / 10 % 10 + '0');
+		_putchar(hn % 10 + '0');
+	}
+		hn = n % 100000;
+		if (hn / 10000 % 10 != 0)
+		{
+			_putchar(hn / 10000 % 10 + '0');
+			_putchar(hn / 1000 % 10 + '0');
+			_putchar(hn / 100 % 10 + '0');
+			_putchar(hn / 10 % 10 + '0');
+		}
+		else if (hn / 1000 % 10 != 0)
+		{
+			_putchar(hn / 1000 % 10 + '0');
+			_putchar(hn / 100 % 10 + '0');
+			_putchar(hn / 10 % 10 + '0');
+		}
+		else if (hn / 100 % 10 != 0)
+		{
+			_putchar(hn / 100 % 10 + '0');
+			_putchar(hn / 10 % 10 + '0');
+		}
+		else if (hn / 10 % 10 != 0)
+			_putchar(hn / 10 % 10 + '0');
+		_putchar(hn % 10 + '0');
+}
