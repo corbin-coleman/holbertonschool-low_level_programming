7426f7f4b5f7245f9f2fa381e558ffcfad2f9417
Corbin <corbin.coleman@holbertonschool.com>
2016-10-20 17:36:07 +0000

    Task 9 - Print 0-100, print Fizz, Buzz, or FizzBuzz for multiples of 3, 5, or both

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n = 1;
+
+	while (n <= 100)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+			printf("FizzBuzz");
+		else if (n % 3 == 0)
+			printf("Fizz");
+		else if (n % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", n);
+		if (n != 100)
+			printf(" ");
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
