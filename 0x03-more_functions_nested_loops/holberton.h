a1ac1537ff85ac4f23518ba7f4bc31e964eab38a
Corbin <corbin.coleman@holbertonschool.com>
2016-10-20 16:52:57 +0000

    Task 4 - Print 0-9 except 2 & 4

--- 0x03-more_functions_nested_loops/holberton.h
+++ 0x03-more_functions_nested_loops/holberton.h
@@ -15,3 +15,4 @@ int _isupper(int);
 int _isdigit(int);
 int mul(int, int);
 void print_numbers(void);
+void print_most_numbers(void);
--- 0x03-more_functions_nested_loops/main.c
+++ 0x03-more_functions_nested_loops/main.c
@@ -8,6 +8,6 @@
  */
 int main(void)
 {
-	print_numbers();
+	print_most_numbers();
 	return (0);
 }
