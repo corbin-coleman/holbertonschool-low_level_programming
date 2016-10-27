ce1ee54e031a4fe41e8d18b1d32f63e7a0671ba8
Corbin <corbin.coleman@holbertonschool.com>
2016-10-27 23:31:46 +0000

    Task 8 - Encode a string using rot13

--- 0x05-pointers_arrays_strings/8-rot13.c
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -6,13 +6,26 @@
  */
 char *rot13(char *s)
 {
-	int i, j;
+	int i;
 
 	i = 0;
 	while (s[i] != 0)
 	{
-		
-		i++;
+		if ((s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z'))
+		{
+			while ((s[i] >= 'A' && s[i] < 'N') || (s[i] >= 'a' && s[i] < 'n'))
+			{
+				s[i] += 13;
+				i++;
+			}
+			while ((s[i] >= 'N' && s[i] <= 'Z') || (s[i] >= 'n' && s[i] <= 'z'))
+			{
+				s[i] -= 13;
+				i++;
+			}
+		}
+		else
+			i++;
 	}
 	return (s);
 }
