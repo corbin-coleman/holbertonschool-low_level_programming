61b95ab0cfa99041dfb13c52f6ebfb650b77b239
Corbin <corbin.coleman@holbertonschool.com>
2016-10-27 20:28:02 +0000

    Task 3 - Proper comparisons

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -7,21 +7,36 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int l1, l2;
+	int i, l1, l2;
 
-	l1 = l2 = 0;
-	while (s1[l1] != '\0')
-	{
+	i = l1 = l2 = 0;
+	while (s1[l1] != 0)
 		l1++;
-	}
-	while (s2[l2] != '\0')
-	{
+	while (s2[l2] != 0)
 		l2++;
+	if (l1 >= l2)
+	{
+		while (s2[i] != 0)
+		{
+			if (s1[i] < s2[i])
+				return (-1);
+			else if (s1[i] > s2[i])
+				return (1);
+			else
+				i++;
+		}
 	}
-	if (l1 < l2)
-		return (-15);
-	else if (l1 > l2)
-		return (15);
 	else
-		return (0);
+	{
+		while (s1[i] != 0)
+		{
+			if (s1[i] < s2[i])
+				return (-1);
+			else if (s1[i] > s2[i])
+				return (1);
+			else
+				i++;
+		}
+	}
+	return (0);
 }
