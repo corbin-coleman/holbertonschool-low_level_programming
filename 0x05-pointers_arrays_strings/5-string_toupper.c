e06f0afdaff4fa25ca80b84867a1f0355527b7b4
Corbin <corbin.coleman@holbertonschool.com>
2016-10-27 19:42:03 +0000

    Task 5 - Uppercase all lowercase letters in a string

---
+++ 0x05-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+/**
+ * string_toupper - Convert all lowercase letters to uppercase
+ * @s: String to capitalize
+ *
+ * Return: Pointer to string;
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != 0)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+		i++;
+	}
+	return (s);
+}
--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -1,41 +1,13 @@
 #include "holberton.h"
 #include <stdio.h>
 
-/**
- * main - check the code for Holberton School students.
- * @a: an array of integers
- * @n: the number of elements to swap
- *
- * Return: nothing.
- */
-void print_array(int *a, int n)
-{
-	int i;
-
-	i = 0;
-	while (i < n)
-	{
-		if (i != 0)
-		{
-			printf(", ");
-		}
-		printf("%d", a[i]);
-		i++;
-	}
-	printf("\n");
-}
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
 int main(void)
 {
-	int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
+	char s[] = "Holberton School!\n";
+	char *p;
 
-	print_array(a, sizeof(a) / sizeof(int));
-	reverse_array(a, sizeof(a) / sizeof(int));
-	print_array(a, sizeof(a) / sizeof(int));
+	p = string_toupper(s);
+	printf("%s", p);
+	printf("%s", s);
 	return (0);
 }
