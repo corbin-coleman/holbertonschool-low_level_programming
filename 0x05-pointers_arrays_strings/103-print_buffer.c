06c8569c9be54e8e712fc5a2ce4eb35220b7a263
Corbin <corbin.coleman@holbertonschool.com>
2016-10-28 23:48:23 +0000

    Task 12 - Print buffer, proper position output

--- 0x05-pointers_arrays_strings/103-print_buffer.c
+++ 0x05-pointers_arrays_strings/103-print_buffer.c
@@ -10,33 +10,38 @@ void print_buffer(char *b, int size)
 	int i, j;
 
 	i = 0;
-	while (i < size)
-	{
-		printf("%p: ", &b[i]);
-		j = 0;
-		while (j < 10 )
-		{
-			if (j % 2 == 0 && j > 0)
-			    printf(" ");
-			if (j + i > size - 1)
-				printf("  ");
-			else
-				printf("%.2x", b[j + i]);
-			j++;
-		}
-		putchar(' ');
-		j = 0;
-		while (j < 10)
-		{
-			if (b[j + i] >= ' ' && b[j + i] <= '~')
-				putchar(b[j + i]);
-			else if (j + i > size - 1)
-				break;
-			else
-				putchar('.');
-			j++;
-		}
+	if (size <= 0)
 		putchar('\n');
-		i += 10;
+	else
+	{
+		while (i < size)
+		{
+			printf("%08x: ", i);
+			j = 0;
+			while (j < 10 )
+			{
+				if (j % 2 == 0 && j > 0)
+					printf(" ");
+				if (j + i > size - 1)
+					printf("  ");
+				else
+					printf("%.2x", b[j + i]);
+				j++;
+			}
+			putchar(' ');
+			j = 0;
+			while (j < 10)
+			{
+				if (b[j + i] >= ' ' && b[j + i] <= '~')
+					putchar(b[j + i]);
+				else if (j + i > size - 1)
+					break;
+				else
+					putchar('.');
+				j++;
+			}
+			putchar('\n');
+			i += 10;
+		}
 	}
 }
