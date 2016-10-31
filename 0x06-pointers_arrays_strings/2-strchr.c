975d50467f3a648a8dd9872da81c65a850ab341a
Corbin <corbin.coleman@holbertonschool.com>
2016-10-31 16:40:25 +0000

    Task 2 - Find a given char in a given string

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+/**
+ * _strchr - Find given char in a string
+ * @s: string to search
+ * @c: char to search for
+ *
+ * Return: Pointer to first instance of c
+ */
+
+char *_strchr(char *s, char c)
+{
+	char *f;
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			f = &s[i];
+			return (f);
+		}
+		i++;
+	}
+	return (0);
+}
--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -2,46 +2,18 @@
 #include <stdio.h>
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
- */
-void simple_print_buffer(char *buffer, unsigned int size)
-{
-	unsigned int i;
-
-	i = 0;
-	while (i < size)
-	{
-		if (i % 10)
-		{
-			printf(" ");
-		}
-		if (!(i % 10) && i)
-		{
-			printf("\n");
-		}
-		printf("0x%02x", buffer[i]);
-		i++;
-	}
-	printf("\n");
-}
-
-/**
  * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	char buffer[98] = {0};
-	char buffer2[98] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+	char *s = "hello";
+	char *f;
 
-	simple_print_buffer(buffer, 98);
-	_memcpy(buffer + 50, buffer2, 10);
-	printf("-------------------------------------------------\n");
-	simple_print_buffer(buffer, 98);
+	f = _strchr(s, 'l');
+	printf("%s\n", f);
+	f = _strchr(s, 'a');
+	printf("%s\n", f);
 	return (0);
 }
