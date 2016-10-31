18672781cbc86f83d24d3e69424d1b04ce646ff3
Corbin <corbin.coleman@holbertonschool.com>
2016-10-31 17:28:40 +0000

    Task 6 - Print a chessboard

--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -1,6 +1,5 @@
 #include "holberton.h"
 #include <stdio.h>
-#include <string.h>
 
 /**
  * main - check the code for Holberton School students.
@@ -9,11 +8,16 @@
  */
 int main(void)
 {
-	char *s = "hello, world";
-	char *f = "bob";
-	char *t;
-
-	t = strstr(s, f);
-	printf("%s\n", t);
+	char board[8][8] = {
+		{'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'},
+		{'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+		{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+		{'R', 'K', 'B', 'Q', 'K', 'B', 'K', 'R'},
+	};
+	print_chessboard(board);
 	return (0);
 }
