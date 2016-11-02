57e5be4bd290f3445ddb57643a0923ae40a4424f
Corbin <corbin.coleman@holbertonschool.com>
2016-11-02 19:38:29 +0000

    Task 5 - Betty really fixed

--- 0x07-recursion/5-sqrt_recursion.c
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -1,22 +1,3 @@
-int sqrt_check(int, int);
-
-/**
- * _sqrt_recursion - Find the square root of given number
- * @n: Number given to find root of
- *
- * Return: The square root or -1 if there is no natural square root
- */
-int _sqrt_recursion(int n)
-{
-	if (n == 1)
-		return (1);
-	else if (n < 0)
-		return (-1);
-	else if (n == 0)
-		return (0);
-	return (sqrt_check(n, 1));
-}
-
 /**
  * sqrt_check - Find the square root
  * @n: Number to find square root of
@@ -33,4 +14,21 @@ int sqrt_check(int n, int c)
 	else if (c >= n / 2)
 		return (-1);
 	return (sqrt_check(n, c + 1));
+}
+
+/**
+ * _sqrt_recursion - Find the square root of given number
+ * @n: Number given to find root of
+ *
+ * Return: The square root or -1 if there is no natural square root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 1)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (0);
+	return (sqrt_check(n, 1));
 }
