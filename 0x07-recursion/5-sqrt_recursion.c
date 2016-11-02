34be945629a96c8de36c2af780e062dc24e947ff
Corbin <corbin.coleman@holbertonschool.com>
2016-11-02 19:30:36 +0000

    Task 5 - Find the square root of a number w/ recursion

---
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int sqrt_check(int, int);
+/**
+ * _sqrt_recursion - Find the square root of given number
+ * @n: Number given to find root of
+ *
+ * Return: The square root or -1 if there is no natural square root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 1)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (0);
+	return (sqrt_check(n, 1));
+}
+
+int sqrt_check(int n, int c)
+{
+	if (c * c == n)
+	{
+		return (c);
+	}
+	else if (c >= n/2)
+		return (-1);
+	return (sqrt_check(n, c + 1));
+}
