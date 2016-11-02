f07aa944ae5ce924a9be20c629aa9edc8283a6a3
Corbin <corbin.coleman@holbertonschool.com>
2016-11-02 19:29:32 +0000

    Task 4 - Betty style fixes

--- 0x07-recursion/4-pow_recursion.c
+++ 0x07-recursion/4-pow_recursion.c
@@ -1,3 +1,10 @@
+/**
+ * _pow_recursion - Find the value of x to the power of y
+ * @x: The number to multiple
+ * @y: The power to multiply it to
+ *
+ * Return: Value of x to the power of y
+ */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
