6424876e0e29c17cf1c689e549db60ed3a10d3bc
Corbin <corbin.coleman@holbertonschool.com>
2016-11-02 17:14:26 +0000

    Task 4 - Find value of x to the power y

---
+++ 0x07-recursion/4-pow_recursion.c
@@ -0,0 +1,10 @@
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else if (y > 0)
+		return (x * _pow_recursion(x, y - 1));
+	return (x * _pow_recursion(x, y - 1));
+}
