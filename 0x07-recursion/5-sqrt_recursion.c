3a79de97a8b7adabf7fc87c743687b8aed608668
Corbin <corbin.coleman@holbertonschool.com>
2016-11-02 19:34:44 +0000

    Task 5 - Betty fixes

--- 0x07-recursion/5-sqrt_recursion.c
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -1,6 +1,5 @@
-#include <stdio.h>
-
 int sqrt_check(int, int);
+
 /**
  * _sqrt_recursion - Find the square root of given number
  * @n: Number given to find root of
@@ -18,13 +17,20 @@ int _sqrt_recursion(int n)
 	return (sqrt_check(n, 1));
 }
 
+/**
+ * sqrt_check - Find the square root
+ * @n: Number to find square root of
+ * @c: The number checked to see if it's the sqrt of n
+ *
+ * Return: The square root value
+ */
 int sqrt_check(int n, int c)
 {
 	if (c * c == n)
 	{
 		return (c);
 	}
-	else if (c >= n/2)
+	else if (c >= n / 2)
 		return (-1);
 	return (sqrt_check(n, c + 1));
 }
