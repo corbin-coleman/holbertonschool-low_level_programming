b4ea1be01172401f8feec6a3b10cde1c88ebada1
Corbin <corbin.coleman@holbertonschool.com>
2016-11-04 21:29:49 +0000

    Task 5 - Print the smallest number of coins needed to give change

---
+++ 0x09-argc_argv/100-change.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ * main - Find the smallest number of coins to make an amount of change
+ * @argc: Arg count
+ * @argv: Contains all arguments
+ *
+ * Return: 1 on error, 0 for success
+ */
+int main(int argc, char *argv[])
+{	int cents, coins = 0;
+
+	if (argc == 2)
+	{
+		cents = atoi(*(argv + 1));
+		while (cents > 0)
+		{
+			if (cents % 25 < cents)
+			{
+				cents -= 25;
+				coins++;
+			}
+			else if (cents % 10 < cents)
+			{
+				cents -= 10;
+				coins++;
+			}
+			else if (cents % 5 < cents)
+			{
+				cents -= 5;
+				coins++;
+			}
+			else if (cents % 2 < cents)
+			{
+				cents -= 2;
+				coins++;
+			}
+			else if (cents % 1 < cents)
+			{
+				cents -= 1;
+				coins++;
+			}
+		}
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", coins);
+	return (0);
+}
