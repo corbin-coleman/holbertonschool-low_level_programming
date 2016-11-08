0e6b724c8f1bf998e572b86154154ae5d0eb75d3
Corbin <corbin.coleman@holbertonschool.com>
2016-11-08 20:27:08 +0000

    Task 3 - Create a 2D array initialized w/ 0s

---
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * alloc_grid - Create a 2 dimensional array
+ * @width: Width of grid, aka # of columns
+ * @height: Height of grid, aka # of rows
+ *
+ * Return: Pointer to 2D array, NULL if it fails
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i, j;
+
+	i = 0;
+	grid = malloc(height * sizeof(height));
+	if (grid == NULL)
+		return (NULL);
+	while (i < height)
+	{
+		j = 0;
+		grid[i] = malloc(width * sizeof(width));
+		if (grid[i] == NULL)
+			return (NULL);
+		while (j < width)
+		{
+			grid[i][j] = 0;
+			j++;
+		}
+		i++;
+	}
+	return (grid);
+}
--- 0x0A-malloc_free/holberton.h
+++ 0x0A-malloc_free/holberton.h
@@ -1,3 +1,4 @@
+int **alloc_grid(int width, int height);
 char *str_concat(char *s1, char *s2);
 char *_strdup(char *str);
 char *create_array(unsigned int size, char c);
