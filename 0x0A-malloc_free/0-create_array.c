0308c2cf1066707c3110a749d7eec3b1fa3afed6
Corbin <corbin.coleman@holbertonschool.com>
2016-11-08 17:50:43 +0000

    Task 0 - Create an array of char of given size and initialize with given character

---
+++ 0x0A-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+/**
+ * create_aray - create array of chars initialized w/ specific char
+ * @size: Size of the array
+ * @c: Char to initialize with
+ *
+ * Return: Pointer to array, or NULL if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ar;
+	unsigned int i;
+
+	i = 0;
+	ar = malloc(size * sizeof(c));
+	if (ar == NULL)
+		return (NULL);
+	while (i < size)
+	{
+		ar[i] = c;
+		i++;
+	}
+	return (ar);
+}
