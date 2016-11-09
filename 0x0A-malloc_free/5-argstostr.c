bd26c06b299715b37c376175fccfedacab570b76
Corbin <corbin.coleman@holbertonschool.com>
2016-11-09 22:20:43 +0000

    Task 5 - Combine all arguments into a string with each separated by new line

--- 0x0A-malloc_free/5-argstostr.c
+++ 0x0A-malloc_free/5-argstostr.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
  * argstostr - Concatenate all arguments of a program
@@ -45,18 +44,4 @@ char *argstostr(int ac, char **av)
 		i++;
 	}
 	return (str);
-}
-
-int main(int ac, char *av[])
-{
-	char *s;
-
-	s = argstostr(ac, av);
-	if (s == NULL)
-	{
-		return (1);
-	}
-	printf("%s", s);
-	free(s);
-	return (0);
 }
