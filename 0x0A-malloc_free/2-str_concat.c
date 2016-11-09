79481a7dd892d61742d87b48f95652693c043504
Corbin <corbin.coleman@holbertonschool.com>
2016-11-09 18:12:17 +0000

    Task 2 - Combine two given strings

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -13,17 +13,15 @@ char *str_concat(char *s1, char *s2)
 	int i, len1, len2, j;
 
 	i = len1 = len2 = 0;
-	while (s1[i] != '\0')
+	while (s1[len1] != '\0')
 	{
 		len1++;
-		i++;
 	}
-	i = 0;
-	while (s2[i] != '\0')
+	while (s2[len2] != '\0')
 	{
 		len2++;
-		i++;
 	}
+	len2++;
 	s3 = malloc((len1 + len2) * sizeof(*s1));
 	if (s3 == NULL)
 		return (NULL);
@@ -34,7 +32,7 @@ char *str_concat(char *s1, char *s2)
 		i++;
 	}
 	j = 0;
-	while (j < len2)
+	while (j <= len2)
 	{
 		s3[i] = s2[j];
 		i++;
