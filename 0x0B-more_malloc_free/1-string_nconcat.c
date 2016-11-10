da4a6a8d3f5a521e6d89369a6d3aa1995fa7a897
Corbin <corbin.coleman@holbertonschool.com>
2016-11-10 18:05:14 +0000

    Task 1 - Concatenate 2 strings, using only n bytes of the second string

---
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+
+/**
+ * string_nconcat - Concatenate two strings using n amount of s2
+ * @s1: First string
+ * @s2: String to add to end of s1
+ * @n: Amount of s2 to add to s1
+ *
+ * Return: pointer to new area in memory, NULL if it fails
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *nstr;
+	unsigned int i, len, j;
+	unsigned int size;
+
+	len = 0;
+	while (s1[len] != '\0')
+		len++;
+	size = (len + n) * sizeof(*nstr);
+	nstr = malloc(size);
+	if (nstr == NULL)
+		return (NULL);
+	i = 0;
+	while (i < size && s1[i] != '\0')
+	{
+		nstr[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (i < size && s2[j] != '\0')
+	{
+		nstr[i] = s2[j];
+		i++;
+		j++;
+	}
+	nstr[i] = '\0';
+	return (nstr);
+}
