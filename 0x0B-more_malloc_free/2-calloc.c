bd2116cb9bd41d3890a3fcb1bf48ed95027e2623
Corbin <corbin.coleman@holbertonschool.com>
2016-11-10 18:27:40 +0000

    Task 2 - Allocate memory for an array using malloc, set memory to zero (copies functionality of calloc())

---
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+/**
+ * _calloc - Allocate memory for array of nmemb elements of size bytes
+ * @nmemb: Number of elemnts
+ * @size: Size in bytes of elements
+ *
+ * Return: Pointer to new memory, NULL if it fails
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ar;
+	unsigned int ar_size, i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ar_size = nmemb * size;
+	ar = malloc(ar_size);
+	if (ar == NULL)
+		return (NULL);
+	while (i < ar_size)
+	{
+		ar[i] = 0;
+		i++;
+	}
+	return (ar);
+}
