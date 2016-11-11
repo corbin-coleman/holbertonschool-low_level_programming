d037a0556ec1f95da3552819969ab9658253a559
Corbin <corbin.coleman@holbertonschool.com>
2016-11-11 05:19:54 +0000

    Task 5 - Multiply two given arguments

--- 0x0B-more_malloc_free/101-mul.c
+++ 0x0B-more_malloc_free/101-mul.c
@@ -1,17 +1,5 @@
 #include <stdlib.h>
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1
- * On error, -1 is returned, and errno is set appropriately
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+#include "holberton.h"
 
 /**
  * print_str - Print a given string
