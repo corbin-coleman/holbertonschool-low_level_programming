3b5c1267ff8d6b5c174be0916becc4f909b4aa64
Corbin <corbin.coleman@holbertonschool.com>
2016-11-14 20:31:43 +0000

    Task 3 - Redefine struct with typedef

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,6 +1,14 @@
-struct dog
+/**
+ * struct dog - Create a struct of a dog
+ * @name: Dog's name
+ * @age: Age of the dog
+ * @owner: Dog's owner
+ */
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
