0d75d97cd5edfdd277472f4f5c8902d13270f9f7
Corbin <corbin.coleman@holbertonschool.com>
2016-11-14 21:22:08 +0000

    Task 4 - Create a new dog struct

---
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - Create a new dog struct
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ *
+ * Return: pointer to struct, NULL if fails
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+	return (new_dog);
+}
