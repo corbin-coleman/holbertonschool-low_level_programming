90142bd5f197365626caf58e78eb4fe791fdc274
Corbin <corbin.coleman@holbertonschool.com>
2016-11-14 19:11:49 +0000

    Task 1 - Write a function to initialize struct dog

---
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * init_dog - Initialize the struct dog
+ * @d: pointer to struct
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Name of owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
