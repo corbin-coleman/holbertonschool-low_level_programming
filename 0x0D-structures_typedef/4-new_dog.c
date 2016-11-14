794102f074cab5c0f7546fef245db7938481bdc6
Corbin <corbin.coleman@holbertonschool.com>
2016-11-14 21:58:31 +0000

    Task 5 & 4

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -2,6 +2,25 @@
 #include <stdlib.h>
 
 /**
+ * *_strcpy - Copy the given string
+ * @dest: Location to copy string to
+ * @src: String to copy to given location
+ *
+ * Return: Pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*(src + i) != 0)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+}
+
+/**
  * new_dog - Create a new dog struct
  * @name: Name of dog
  * @age: Age of dog
@@ -12,12 +31,22 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
+	char *new_name;
+	char *new_owner;
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
-	new_dog->name = name;
+	new_name = malloc(sizeof(name));
+	if (new_name == NULL)
+		return (NULL);
+	new_owner = malloc(sizeof(owner));
+	if (new_owner == NULL)
+		return (NULL);
+	new_name = _strcpy(new_name, name);
+	new_owner = _strcpy(new_owner, owner);
+	new_dog->name = new_name;
 	new_dog->age = age;
-	new_dog->owner = owner;
+	new_dog->owner = new_owner;
 	return (new_dog);
 }
