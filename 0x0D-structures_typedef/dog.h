dbf8b8842fb9f7dd9842713d390ae1b6993417b2
Corbin <corbin.coleman@holbertonschool.com>
2016-11-14 21:44:45 +0000

    Header update

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -6,12 +6,16 @@
  * @age: Age of the dog
  * @owner: Dog's owner
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
+
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
