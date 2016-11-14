190e92547eaa94d29cf71d73945f6a40d75d3103
Corbin <corbin.coleman@holbertonschool.com>
2016-11-14 20:58:27 +0000

    Task 0 - Define a struct dog

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,13 +1,17 @@
+#ifndef DOG
+#define DOG
 /**
  * struct dog - Create a struct of a dog
  * @name: Dog's name
  * @age: Age of the dog
  * @owner: Dog's owner
  */
-typedef struct dog {
+typedef struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 } dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+#endif
