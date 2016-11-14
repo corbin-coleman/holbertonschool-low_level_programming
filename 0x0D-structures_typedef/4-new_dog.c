4c3483133b5a8ebc24577591196f828f3054649a
Corbin <corbin.coleman@holbertonschool.com>
2016-11-14 21:40:03 +0000

    Task 4 - Create new dog

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -13,7 +13,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
 
-	new_dog = malloc(sizeof(dog_t));
+	new_dog = malloc(sizeof(struct dog_t));
 	if (new_dog == NULL)
 		return (NULL);
 	new_dog->name = name;
