3a81a00f3f3541834389c95471fdb8e23d87dae2
Corbin <corbin.coleman@holbertonschool.com>
2016-11-15 23:07:09 +0000

    Task 4 - Free all the mistakes

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -56,7 +56,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_owner = malloc(len * sizeof(*name));
 	if (new_owner == NULL)
 	{
-		free(name);
+		free(new_name);
 		free(new_dog);
 		return (NULL);
 	}
