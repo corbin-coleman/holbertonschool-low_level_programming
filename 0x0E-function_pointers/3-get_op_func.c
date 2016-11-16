a7073163743aa50c6404cb2a38553f232ddddd04
Corbin <corbin.coleman@holbertonschool.com>
2016-11-16 21:21:43 +0000

    Fixing weird betty check

--- 0x0E-function_pointers/3-get_op_func.c
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -4,14 +4,17 @@
 /**
  * get_op_func - Find the proper operator to call the proper function
  * @s: Operator to determine proper function to call
- *
  * Return: Pointer to function matching operator
  */
 int (*get_op_func(char *s))(int, int)
 {
-	char *ops = "+-*/%";
-	int (*func[])(int, int) = {op_add, op_sub, op_mul, op_div, op_mod};
 	int i;
+
+	char *ops = "+-*/%";
+
+	int (*func[])(int, int) = {
+		op_add, op_sub, op_mul, op_div, op_mod
+	};
 
 	i = 0;
 	while (ops[i] != '\0')
