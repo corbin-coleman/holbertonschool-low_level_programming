dc8ebbe19b0fa53153e59c1dbe106cd4547931b9
Corbin <corbin.coleman@holbertonschool.com>
2016-11-16 17:34:58 +0000

    Task 1 - Iterate through an array and call a funciton on each element

--- 0x0E-function_pointers/function_pointers.h
+++ 0x0E-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef FUNCTIONS
 #define FUNCTIONS
+void array_iterator(int *array, size_t size, void (*action)(int));
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 #endif
