0a4da5f31ba75649257bc58c5cd426d5e00f176e
Corbin <corbin.coleman@holbertonschool.com>
2016-11-16 17:44:06 +0000

    Task 2 - Search through an array for an integer using given function on array elements

---
+++ 0x0E-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+/**
+ * int_index - Search for an integer
+ * @array: Array to search through
+ * @size: Size of array
+ * @cmp: Compare function to use
+ *
+ * Return: Index of first element that cmp does not return 0, -1 if
+ * no elements are reached or size <= 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0)
+		return (-1);
+	i = 0;
+	while (i < size)
+	{
+		if (cmp(array[i]))
+			return (i);
+		i++;
+	}
+	return (-1);
+}
