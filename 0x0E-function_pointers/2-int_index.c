82b24fde5d92a144178b9376680c0efab9b7646d
Corbin <corbin.coleman@holbertonschool.com>
2016-11-16 21:09:50 +0000

    Task 2 - Find element in array based on function called by pointer

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 /**
  * int_index - Search for an integer
  * @array: Array to search through
@@ -11,6 +13,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
+	if (array == NULL)
+		return (-1);
+	if (cmp == NULL)
+		return (-1);
 	if (size <= 0)
 		return (-1);
 	i = 0;
