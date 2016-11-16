4a0897e0bfd436f51449f888ae9ce47dc2e79d81
Corbin <corbin.coleman@holbertonschool.com>
2016-11-16 21:28:55 +0000

    Fixing double operator error

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -1,4 +1,4 @@
-#include "calc.h"
+#include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -17,6 +17,11 @@ int main(int argc, char *argv[])
 
 	if (argc == 4)
 	{
+		if (argv[2][1] != '\0')
+		{
+			printf("Error\n");
+			exit(98);
+		}
 		func = get_op_func(argv[2]);
 		if (func == NULL)
 		{
