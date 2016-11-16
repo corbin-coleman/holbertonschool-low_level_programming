75e2c8e673426d34e6a06a1dc032e81298bd52ce
Corbin <corbin.coleman@holbertonschool.com>
2016-11-16 21:13:44 +0000

    Adding calc header file, and fixing division by 0

--- 0x0E-function_pointers/3-op_functions.c
+++ 0x0E-function_pointers/3-op_functions.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * op_add - Return sum of a & b
  * @a: First int to add
@@ -43,6 +46,11 @@ int op_mul(int a, int b)
  */
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
 
@@ -55,5 +63,10 @@ int op_div(int a, int b)
  */
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
