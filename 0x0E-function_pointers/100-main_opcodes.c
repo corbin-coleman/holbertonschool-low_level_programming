6e4ed628496302ae5751418191824410235edfcf
Corbin <corbin.coleman@holbertonschool.com>
2016-11-17 18:55:58 +0000

    Task 4 - Print out 2 digit hex opcodes of the main function

---
+++ 0x0E-function_pointers/100-main_opcodes.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Print the opcodes of the function
+ * @argc: Number of args given
+ * @argv: Argument of number of bytes to print
+ *
+ * Return: 0 on success, Print Error for errors, exit value 1 if
+ * incorrect # of args, exit value 2 if number of bytes is negative
+ */
+int main(int argc, char *argv[])
+{
+	int (*ptr)(int, char**);
+	char *add;
+	int i, bytes;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	ptr = main;
+	add = (char *)&ptr;
+	bytes = atoi(argv[1]);
+	i = 0;
+	while (i < bytes - 1)
+	{
+		printf("%02hhx ", add[i]);
+		i++;
+	}
+	printf("%02hhx\n", add[i]);
+	return (0);
+}
