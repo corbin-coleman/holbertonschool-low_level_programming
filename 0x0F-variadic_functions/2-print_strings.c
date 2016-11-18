5885c06f99c65fcd34185df896f6f436bea60797
Corbin <corbin.coleman@holbertonschool.com>
2016-11-18 21:06:09 +0000

    Task 2 - Variadic function to print any number of given strings

---
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list words;
+	char *word;
+	unsigned int i;
+
+	va_start(words, n);
+	i = 0;
+	while (i < n)
+	{
+		word = va_arg(words, char *);
+		if (word != NULL)
+			printf("%s", word);
+                else
+                        printf("(nil)");
+		if (i < n - 1 && separator != NULL)
+			printf("%s", separator);
+		i++;
+	}
+	printf("\n");
+	va_end(words);
+}
--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
 #ifndef VARIADIC_FUNCS
 #define VARIADIC_FUNCS
+void print_strings(const char *separator, const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 #endif
