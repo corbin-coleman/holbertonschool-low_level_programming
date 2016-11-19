2c0e54428a32b7d95ca6975016cbde3d3b1c7ab5
Corbin <corbin.coleman@holbertonschool.com>
2016-11-19 00:33:50 +0000

    Task 3 - Variadic function prints out any give arg

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -2,6 +2,33 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+int format_len(const char * const format)
+{
+	int i, len;
+
+	i = len = 0;
+	while (format[i] != '\0')
+	{
+		switch (format[i])
+		{
+		case 'c':
+			len++;
+			break;
+		case 'i':
+			len++;
+			break;
+		case 'f':
+			len++;
+			break;
+		case 's':
+			len++;
+			break;
+		}
+		i++;
+	}
+	return (len);
+}
+
 /**
  * print_all - Print any type of given argument
  * @format: List of types of arguments being passed
@@ -9,43 +36,43 @@
 void print_all(const char * const format, ...)
 {
 	va_list list;
-	int i, len, print;
+	char *word;
+	int i, len, print, lenc;
 
-	i = len = 0;
-	while (format[i] != '\0')
-	{
-		if (format[i] == 'c' || format[i] == 'i' || format[i] == 'f'
-		    || format[i] == 's')
-			len++;
-		i++;
-	}
-	i  = 0;
+	len = format_len(format);
+	i = lenc = 0;
 	va_start(list, format);
 	while (format[i] != '\0')
 	{
 		print = 0;
 		switch (format[i])
 		{
-		case 'c' :
+		case 'c':
 			printf("%c", va_arg(list, int));
-			print = 1;
+			lenc++;	print = 1;
 			break;
-		case 'i' :
+		case 'i':
 			printf("%d", va_arg(list, int));
-			print = 1;
+			lenc++;	print = 1;
 			break;
-		case 'f' :
+		case 'f':
 			printf("%f", va_arg(list, double));
-			print = 1;
+			lenc++; print = 1;
 			break;
-		case 's' :
-			printf("%s", va_arg(list, char *));
+		case 's':
+			word = va_arg(list, char *);
+			if (word == NULL)
+				printf("(nil)");
+			else
+				printf("%s", word);
+			lenc++;
 			print = 1;
 			break;
 		}
-		if (print == 1 && format[i + 1] != '\0')
+		if (print == 1 && format[i + 1] != '\0' && lenc != len)
 			printf(", ");
 		i++;
 	}
 	printf("\n");
+	va_end(list);
 }
