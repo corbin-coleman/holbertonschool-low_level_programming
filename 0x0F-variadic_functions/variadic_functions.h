7bcb2b21ea9e71586ca71068d5c83a37c8e250c1
Corbin <corbin.coleman@holbertonschool.com>
2016-11-20 00:57:31 +0000

    Addiing betty documentation for header file

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,5 +1,11 @@
 #ifndef VARIADIC_FUNCS
 #define VARIADIC_FUNCS
+#define FORMAT(f) ("%"#f)
+/**
+ * print_form - Define a struct to figure out how to print
+ * @c: Character to check for
+ * @f: Pointer to function to call
+ */
 typedef struct print_form
 {
 	char *c;
