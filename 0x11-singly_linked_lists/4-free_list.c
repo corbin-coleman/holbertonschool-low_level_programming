80365fae70f58f498ea1ee5900eaba8af1fa25cf
Corbin <corbin.coleman@holbertonschool.com>
2016-11-28 22:00:51 +0000

    Task 4 - Free a list_t list

---
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - Free all malloced spaced of a list
+ * @head: Pointer to the start of the list
+ */
+void free_list(list_t *head)
+{
+	if (head->next != NULL)
+		free_list(head->next);
+	free(head->str);
+	free(head);
+}
