b66dfa048b2ea009ce1010a206adec337c099dfa
Corbin <corbin.coleman@holbertonschool.com>
2016-11-28 22:03:18 +0000

    Task 4 - Free a list_t list

--- 0x11-singly_linked_lists/4-free_list.c
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -7,8 +7,11 @@
  */
 void free_list(list_t *head)
 {
-	if (head->next != NULL)
-		free_list(head->next);
-	free(head->str);
-	free(head);
+	if (head != NULL)
+	{
+		if (head->next != NULL)
+			free_list(head->next);
+		free(head->str);
+		free(head);
+	}
 }
