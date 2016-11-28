76d0b696817e55b38616c882794eceb72eb438a1
Corbin <corbin.coleman@holbertonschool.com>
2016-11-28 19:59:50 +0000

    Task 0 - Print all the elements of a list made up of list_t

---
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * print_list - Print all elements of a list_t list
+ * @h: Pointer to the first element of the list
+ *
+ * Return: Number of elements in the list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count;
+
+	count = 1;
+	printf("[%d] %s\n", h->len, h->str);
+	if (h->next != NULL)
+		count += print_list(h->next);
+	return (count);
+}
