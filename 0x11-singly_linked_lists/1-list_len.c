787b709e5e16663618786f4b9926d844ab9890cb
Corbin <corbin.coleman@holbertonschool.com>
2016-11-28 20:00:07 +0000

    Task 1 - Return the number of elements in a list_t list

---
+++ 0x11-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - Returns number of elements in a list
+ * @h: Pointer to first element in list
+ *
+ * Return: Number of elements in the list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count;
+
+	count = 0;
+	count++;
+	if (h->next != NULL)
+		count += list_len(h->next);
+	return (count);
+}
