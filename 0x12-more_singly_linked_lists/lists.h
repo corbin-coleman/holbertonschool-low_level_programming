e723bd74746d2320eb891b3097260aa35ce45ee8
Corbin <corbin.coleman@holbertonschool.com>
2016-11-30 19:03:10 +0000

    Updated for Task 2

--- 0x12-more_singly_linked_lists/lists.h
+++ 0x12-more_singly_linked_lists/lists.h
@@ -1,6 +1,5 @@
 #ifndef LISTS_H
 #define LISTS_H
-#include <stdio.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -17,4 +16,5 @@ typedef struct listint_s
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
 #endif
