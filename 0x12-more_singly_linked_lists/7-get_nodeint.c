7770a77aec5415a3297c3d363d97974116cef5af
Corbin <corbin.coleman@holbertonschool.com>
2016-11-30 20:22:53 +0000

    Task 7 - Find a node based on an index and return that node

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -1,6 +1,13 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ * get_nodeint_at_index - Find a node at a given index in a list
+ * @head: Pointer to the start of the list
+ * @index: The index of the node to look for
+ *
+ * Return: The node, NULL if the node doesn't exist
+ */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int count;
