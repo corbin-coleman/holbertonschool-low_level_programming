1afa354a50250b5128a49a144ce6d4edb55c6093
Corbin <corbin.coleman@holbertonschool.com>
2016-11-30 21:28:57 +0000

    Task 10 - Delete a node at a given index in the list

---
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - Delete a node at a given index in a list
+ * @head: Pointer to a pointer to the start of the list
+ * @index: The location in the list of the node to delete
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *current;
+	listint_t *previous;
+	unsigned int count;
+
+	count = 0;
+	current = *head;
+	if (*head == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		*head = current->next;
+		free(current);
+		return (1);
+	}
+	while (count != index)
+	{
+		previous = current;
+		current = current->next;
+		count++;
+	}
+	previous->next = current->next;
+	free(current);
+	return (1);
+}
