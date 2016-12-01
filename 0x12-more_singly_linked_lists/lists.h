b77a8c535b6af1d708b2421331012ead2d479405
Corbin <corbin.coleman@holbertonschool.com>
2016-12-01 03:02:33 +0000

    Updated for Task 13

--- 0x12-more_singly_linked_lists/lists.h
+++ 0x12-more_singly_linked_lists/lists.h
@@ -37,4 +37,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 listint_t *find_listint_loop(listint_t *head);
+size_t free_listint_safe(listint_t **h);
+listadd_t *add_nodeaddress(listadd_t **head, const listint_t *address);
+void free_listadd(listadd_t *head);
 #endif
