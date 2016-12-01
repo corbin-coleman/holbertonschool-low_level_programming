5d519ec027267def8e9e9bc7cbe8fb27cfa0534e
Corbin <corbin.coleman@holbertonschool.com>
2016-12-01 03:02:55 +0000

    Task 13 - Free any given list, whether it loops or not

---
+++ 0x12-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint_safe - Free a list that may or may not loop,
+ * set start of list to NULL
+ * @h: Pointer to pointer to the start of the list
+ * Return: Size of the list that has been freed
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *killnode;
+        listint_t *current;
+	listadd_t *headadd;
+	listadd_t *checker;
+	size_t count;
+
+	count = 0;
+        current = *h;
+	headadd = NULL;
+        while (current != NULL)
+        {
+                killnode = current;
+		add_nodeaddress(&headadd, current);
+		checker = headadd;
+		while (checker != NULL)
+		{
+			if (current->next == checker->address)
+			{
+				free(current);
+				free_listadd(headadd);
+				headadd = NULL;
+				*h = NULL;
+				return (count);
+			}
+			checker = checker->next;
+		}
+                current = current->next;
+                free(killnode);
+		count++;
+        }
+	free_listadd(headadd);
+	headadd = NULL;
+        *h = NULL;
+	return (count);
+}
