b40a8807ee80bdc6cd0065155f70597871bd7a38
Corbin <corbin.coleman@holbertonschool.com>
2016-12-01 01:22:01 +0000

    Task 12 print a list that may or may not have a loop, go through the list only once.

--- 0x12-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x12-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,6 +3,27 @@
 #include "lists.h"
 
 /**
+ * add_nodeaddress - Add a node to a list with
+ * @head: Pointer to the pointer of the start of the list
+ * @address: The address of another list's node
+ *
+ * Return: Address of new node, NULL if it fails
+ */
+listadd_t *add_nodeaddress(listadd_t **head, const listint_t *address)
+{
+	listadd_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->address = address;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
+
+
+/**
  * print_listint_safe - Print out a given list, but only once if it loops
  * @head: Pointer to the start of the list
  *
@@ -10,27 +31,26 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *addresses[100000];
-	int count, i, j;
+	listadd_t *newhead;
+	listadd_t *checker;
+	unsigned int count;
 
-	printf("[%p] %d\n", (void *)head, head->n);
-	head = head->next;
-	i = 0;
+	count = 0;
+	newhead = NULL;
 	while (head != NULL)
 	{
-		j = 0;
 		printf("[%p] %d\n", (void *)head, head->n);
-		addresses[i] = head;
-		i++;
-		while (j < i)
+		add_nodeaddress(&newhead, head);
+		checker = newhead;
+		while (checker != NULL)
 		{
-			if (head->next == addresses[j])
+			if (head->next == checker->address)
 			{
 				head = head->next;
 				printf("-> [%p] %d\n", (void *)head, head->n);
 				exit(98);
 			}
-			j++;
+			checker = checker->next;
 		}
 		head = head->next;
 		count++;
