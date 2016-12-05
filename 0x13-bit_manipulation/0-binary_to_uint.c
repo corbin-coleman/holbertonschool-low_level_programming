b3df08acbd79c7826800fafec93f472a4aa4c318
Corbin <corbin.coleman@holbertonschool.com>
2016-12-05 20:40:35 +0000

    Task 0 - Convert a string representing a binary number into an unsigned int decimal value

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,14 @@
 #include "holberton.h"
 #include <stdlib.h>
 
+/**
+ * binary_to_uint - Convert a string representing a binary number to an
+ * unsigned int decimal value
+ * @b: The string containing the binary
+ *
+ * Return: 0 if string contains something other than 1 or 0, or
+ * if the string is NULL, return the decimal value on success
+ */
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int i, len;
@@ -18,7 +26,7 @@ unsigned int binary_to_uint(const char *b)
 	}
 	while (i < len)
 	{
-		decval = decval<<1;
+		decval = decval << 1;
 		if (b[i] == '1')
 			decval += 1;
 		i++;
