e183ca3403f2530f7acb63c9609115392f88468f
Corbin <corbin.coleman@holbertonschool.com>
2016-12-06 02:41:46 +0000

    Updated for Task 4

--- 0x13-bit_manipulation/holberton.h
+++ 0x13-bit_manipulation/holberton.h
@@ -1,5 +1,6 @@
 #ifndef HOLBERTON
 #define HOLBERTON
+int clear_bit(unsigned long int *n, unsigned int index);
 int _putchar(char c);
 int set_bit(unsigned long int *n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);
