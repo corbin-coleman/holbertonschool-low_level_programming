e1e8537aabd3d74539dde1835d49df61d578e1ce
Corbin <corbin.coleman@holbertonschool.com>
2016-12-06 02:25:49 +0000

    Task 2 - Return the value of the bit at a given index

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -12,6 +12,8 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned long int count;
 
 	count = 0;
+	if (index > 63)
+		return (-1);
 	while (n > 0)
 	{
 		if (count == index)
@@ -19,5 +21,7 @@ int get_bit(unsigned long int n, unsigned int index)
 		n = n >> 1;
 		count++;
 	}
+	if (count < index)
+		return (0);
 	return (-1);
 }
