a75d2857a91cd2b71949f1d953fd8fbf93ff63ff
Corbin <corbin.coleman@holbertonschool.com>
2016-12-07 20:29:57 +0000

    Task 1 - Create a file and write in content

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+  * create_file - Create a file with a specified name and content
+  * @filename: Name of the file to create
+  * @text_content: Content to read into the file
+  *
+  * Return: 1 on success, -1 on any failure
+  */
+int create_file(const char *filename, char *text_content)
+{
+	int file, len;
+
+	len = 0;
+	if (filename == NULL)
+		return (-1);
+	file = open(filename, O_CREAT | O_WRONLY, 0600);
+	if (file == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		while (text_content[len] != '\0')
+			len++;
+		write(file, text_content, len);
+	}
+	close(file);
+	return (1);
+}
