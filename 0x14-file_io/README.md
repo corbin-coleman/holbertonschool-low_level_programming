e39b27a07341339ae312fd4f3b352c1f52e84b1b
Corbin <corbin.coleman@holbertonschool.com>
2016-12-07 20:10:44 +0000

    Header and README for all tasks

---
+++ 0x14-file_io/README.md
@@ -0,0 +1,16 @@
+#Holberton School - 0x14-file_io
+Description
+## New commands / functions used:
+``gcc``
+## Helpful Links
+*
+
+## Description of Files
+<h6>0-read_textfile.c</h6>
+
+<h6>1-create_file.c</h6>
+
+<h6>2-append_text_to_file.c</h6>
+
+<h6>3-cp.c</h6>
+
---
+++ 0x14-file_io/holberton.h
@@ -0,0 +1,8 @@
+#ifndef HEADER_H
+#define HEADER_H
+#include <stddef.h>
+size_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+int _putchar(char c);
+#endif
