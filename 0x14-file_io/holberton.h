7e36c4d1ab4f5a3560ba5c852135fa4eef45f7dd
Corbin <corbin.coleman@holbertonschool.com>
2016-12-08 17:54:14 +0000

    Include headers in header file

--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -1,7 +1,7 @@
 #ifndef HEADER_H
 #define HEADER_H
-#include <stddef.h>
-size_t read_textfile(const char *filename, size_t letters);
+#include <sys/types.h>
+ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
