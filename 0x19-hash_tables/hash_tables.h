c4522c940c2f2fb67bc370986e09e67508440e1d
corbin-coleman <corbincol@hotmail.com>
2017-02-02 01:21:01 +0000

    Betty compliant

--- 0x19-hash_tables/hash_tables.h
+++ 0x19-hash_tables/hash_tables.h
@@ -5,31 +5,30 @@
 #include <stdio.h>
 
 /**
- * @key : The key, string
+ * struct hash_node_s - Nodes for the lists in the array
+ * @key: The key, string
  * The key is unique in the HashTable
- *
- * @value : The value corresponding to a key
- *
- * @next : A pointer to the next node of the list
+ * @value: The value corresponding to a key
+ * @next: A pointer to the next node of the list
  **/
 typedef struct hash_node_s
 {
-  char *key;
-  char *value;
-  struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
+ * struct hash_table_s - Hash table struct
  * @size: The size of the array
- *
- * @array: An array of size @size
+ * @array: An array of size size
  * Each cell of this array is a pointer to the first node of a linked list,
  * because we want our HashTable to use a Chaining collision handling
  **/
 typedef struct hash_table_s
 {
-  unsigned long int size;
-  hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
 
