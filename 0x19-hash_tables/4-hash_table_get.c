b133fca238aa95d54a5a191e0812760eb7936599
corbin-coleman <corbincol@hotmail.com>
2017-02-02 01:22:42 +0000

    Check if ht is NULL

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -11,6 +11,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *current_node;
 
+	if (ht == NULL)
+		return (NULL);
 	index = key_index((unsigned char *) key, ht->size);
 	current_node = ht->array[index];
 	while (current_node != NULL)
