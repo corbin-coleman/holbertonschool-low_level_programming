f1be8ec9587a32a6cbdfe9f7d623e72555bd1af4
corbin-coleman <corbincol@hotmail.com>
2017-02-02 01:24:58 +0000

    Task 5 - Print out all keys and values in a hash table

---
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Print all keys and values in a hash table
+ * @ht: The hash table to print
+ **/
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int index;
+	hash_node_t *current_node;
+
+	index = 0;
+	printf("{");
+	while (index < ht->size)
+	{
+		current_node = ht->array[index];
+		while (current_node != NULL)
+		{
+			printf("'%s': '%s'", current_node->key, current_node->value);
+			if (!(index != (ht->size - 1) && current_node->next != NULL))
+				printf(", ");
+			current_node = current_node->next;
+		}
+		index++;
+	}
+	printf("}\n");
+}
