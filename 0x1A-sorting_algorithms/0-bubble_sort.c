dfaa20d555bc5f9cfc4d9b055dcf3551884e9cba
corbin-coleman <corbincol@hotmail.com>
2017-02-23 05:40:56 +0000

    Betty checks!

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -1,10 +1,11 @@
 #include "sort.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- *
- *
- *
+ * bubble_sort - Sort by swapping largest items to end of array
+ * @array: Array to be sorted
+ * @size: Size of the array
  */
 void bubble_sort(int *array, size_t size)
 {
@@ -15,16 +16,16 @@ void bubble_sort(int *array, size_t size)
 
 	tempsize = size;
 	swapped = 1;
-	while(swapped)
+	while (swapped)
 	{
 		swapped = 0;
 		j = 0;
-		while(j < tempsize - 1)
+		while (j < tempsize - 1)
 		{
-			if(array[j] > array[j+1])
+			if (array[j] > array[j + 1])
 			{
-				temp = array[j+1];
-				array[j+1] = array[j];
+				temp = array[j + 1];
+				array[j + 1] = array[j];
 				array[j] = temp;
 				print_array(array, size);
 				swapped = 1;
