670ddf640dd42d07abca2728c423ab137765aed9
corbin-coleman <corbincol@hotmail.com>
2017-03-07 23:59:30 +0000

    Task 0 - Add a node to a binary tree connected directly to the parent

---
+++ 0x1C-binary_trees/0-binary_tree_node.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->parent = parent;
+	if (parent != NULL)
+		parent = new_node;
+	new_node->n = value;
+	return (new_node);
+}
