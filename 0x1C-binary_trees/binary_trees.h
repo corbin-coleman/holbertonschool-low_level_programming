cb966ecad966f7e53c673a6ab27a36d8315bea74
corbin-coleman <corbincol@hotmail.com>
2017-03-10 05:00:33 +0000

    Update header file

--- 0x1C-binary_trees/binary_trees.h
+++ 0x1C-binary_trees/binary_trees.h
@@ -42,4 +42,7 @@ size_t binary_tree_depth(const binary_tree_t *node);
 size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
+
+size_t find_height(binary_tree_t *tree, size_t height);
 #endif /* _BINARY_TREES_H_ */
