4e81baa6cc21f67bf3311b1abae8456a241413b7
corbin-coleman <corbincol@hotmail.com>
2017-03-11 20:22:20 +0000

    balance factor with specialized height function

--- 0x1C-binary_trees/14-binary_tree_balance.c
+++ 0x1C-binary_trees/14-binary_tree_balance.c
@@ -1,5 +1,21 @@
 #include "binary_trees.h"
 
+int find_height(binary_tree_t *tree, int height)
+{
+	int left_height, right_height;
+
+	left_height = right_height = 0;
+	if (tree->left == NULL && tree->right == NULL)
+		return (height);
+	if (tree->left != NULL)
+		left_height = find_height(tree->left, height + 1);
+	if (tree->right != NULL)
+		right_height = find_height(tree->right, height + 1);
+	if (left_height > right_height)
+		return (left_height);
+	return (right_height);
+}
+
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	int left_height, right_height;
