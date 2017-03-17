747bed3c3ab8d4fdd14a81d2d700880f4f5c11ce
corbin-coleman <corbincol@hotmail.com>
2017-03-17 00:56:28 +0000

    Update header file

--- 0x1C-binary_trees/binary_trees.h
+++ 0x1C-binary_trees/binary_trees.h
@@ -46,5 +46,5 @@ int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
-
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 #endif /* _BINARY_TREES_H_ */
