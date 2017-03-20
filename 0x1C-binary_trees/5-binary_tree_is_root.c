44211a0b8f18b7134ae1268e8b78d2d5d1c5938f
corbin-coleman <corbincol@hotmail.com>
2017-03-20 21:24:13 +0000

    Check if node is NULL

--- 0x1C-binary_trees/5-binary_tree_is_root.c
+++ 0x1C-binary_trees/5-binary_tree_is_root.c
@@ -7,7 +7,7 @@
  **/
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node->parent == NULL)
-		return (1);
-	return (0);
+	if (node == NULL || node->parent != NULL)
+		return (0);
+	return (1);
 }
