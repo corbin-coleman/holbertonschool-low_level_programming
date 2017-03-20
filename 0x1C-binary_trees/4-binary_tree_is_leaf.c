5a7483de0e169214ba89b38de883837889e4fb93
corbin-coleman <corbincol@hotmail.com>
2017-03-20 21:16:17 +0000

    I don't know

--- 0x1C-binary_trees/4-binary_tree_is_leaf.c
+++ 0x1C-binary_trees/4-binary_tree_is_leaf.c
@@ -9,7 +9,7 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
-	if (node->left == NULL && node->right == NULL && node->parent != NULL)
+	if (node->left == NULL && node->right == NULL)
 		return (1);
 	return (0);
 }
