f731cbfbbbf3e9ba04ad878ff3df9f7088492ff8
corbin-coleman <corbincol@hotmail.com>
2017-03-20 21:10:24 +0000

    Only free if tree exists

--- 0x1C-binary_trees/3-binary_tree_delete.c
+++ 0x1C-binary_trees/3-binary_tree_delete.c
@@ -1,10 +1,17 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_delete - Delete a binary tree
+ * @tree: The root of the tree to delete
+ **/
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (tree->right != NULL)
-		binary_tree_delete(tree->right);
-	if (tree->left != NULL)
-		binary_tree_delete(tree->left);
-	free(tree);
+	if (tree != NULL)
+	{
+		if (tree->right != NULL)
+			binary_tree_delete(tree->right);
+		if (tree->left != NULL)
+			binary_tree_delete(tree->left);
+		free(tree);
+	}
 }
