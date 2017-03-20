5ec34c142ee22bffdf0ee59aa971b97559270551
corbin-coleman <corbincol@hotmail.com>
2017-03-20 21:04:25 +0000

    Fail if parent is NULL

--- 0x1C-binary_trees/2-binary_tree_insert_right.c
+++ 0x1C-binary_trees/2-binary_tree_insert_right.c
@@ -1,26 +1,31 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_insert_right - Insert a not into the right branch of give node
+ * @parent: Parent node of new node
+ * @value: Value to store in new node
+ * Return: Address of new node, NULL if fails
+ **/
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
+	if (parent == NULL)
+		return (NULL);
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = value;
-	if (parent != NULL)
+	if (parent->right == NULL)
 	{
-		if (parent->right == NULL)
-		{
-			parent->right = new_node;
-			new_node->right = NULL;
-		}
-		else
-		{
-			new_node->right = parent->right;
-			parent->right->parent = new_node;
-			parent->right = new_node;
-		}
+		parent->right = new_node;
+		new_node->right = NULL;
+	}
+	else
+	{
+		new_node->right = parent->right;
+		parent->right->parent = new_node;
+		parent->right = new_node;
 	}
 	new_node->parent = parent;
 	new_node->left = NULL;
