3e53b0300f99eeefb568724c509771957b603b9e
corbin-coleman <corbincol@hotmail.com>
2017-03-20 21:41:16 +0000

    Don't use other function

--- 0x1C-binary_trees/13-binary_tree_nodes.c
+++ 0x1C-binary_trees/13-binary_tree_nodes.c
@@ -1,8 +1,13 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_nodes - Count up all non-leaf nodes in a tree
+ * @tree: The tree to count nodes in
+ * Return: Number of non-leaf nodes
+ **/
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	if (tree != NULL && !(binary_tree_is_leaf(tree)))
+	if (tree != NULL && !(tree->left == NULL && tree->righ == NULL))
 		return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 	return (0);
 }
