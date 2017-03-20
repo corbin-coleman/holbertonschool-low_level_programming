0d77b29c4d1bdba1f55f0b3cebf1f7bee0f71621
corbin-coleman <corbincol@hotmail.com>
2017-03-20 22:03:27 +0000

    Don't use other function files

--- 0x1C-binary_trees/15-binary_tree_is_full.c
+++ 0x1C-binary_trees/15-binary_tree_is_full.c
@@ -1,12 +1,31 @@
 #include "binary_trees.h"
 
+/**
+ * leaf_check - Check if node is a leaf
+ * @node: Node to check
+ * Return: 1 if leaf, 0 if not
+ **/
+int leaf_check(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+	return (0);
+}
+
+/**
+ * binary_tree_is_full - Check to see if a tree is full
+ * @tree: The tree to check for
+ * Return: 1 if full, 0 if not
+ **/
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	if (!(binary_tree_is_leaf(tree)) && (tree->left == NULL || tree->right == NULL))
+	if (!(leaf_check(tree)) && (tree->left == NULL || tree->right == NULL))
 		return (0);
-	if (binary_tree_is_leaf(tree->right) && binary_tree_is_leaf(tree->left))
+	if (leaf_check(tree->left) && leaf_check(tree->right))
 		return (1);
 	if (!(binary_tree_is_full(tree->left)) || !(binary_tree_is_full(tree->right)))
 		return (0);
