e04e833a2d0926d1834f3a7e11c31ac0f5493cbb
corbin-coleman <corbincol@hotmail.com>
2017-03-20 21:13:02 +0000

    Make sure node has parent

--- 0x1C-binary_trees/4-binary_tree_is_leaf.c
+++ 0x1C-binary_trees/4-binary_tree_is_leaf.c
@@ -1,8 +1,15 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_is_leaf - Find if a node of a tree is a leaf
+ * @node: The node to check
+ * Return: 1 if it's a leaf, 0 if it's not
+ **/
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node->left == NULL && node->right == NULL)
+	if (node == NULL)
+		return (0);
+	if (node->left == NULL && node->right == NULL && node->parent != NULL)
 		return (1);
 	return (0);
 }
