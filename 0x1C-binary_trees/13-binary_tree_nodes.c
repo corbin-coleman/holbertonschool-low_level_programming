7378d274705f282cd5ee318a5eba63e54dfdcee1
corbin-coleman <corbincol@hotmail.com>
2017-03-20 21:42:06 +0000

    Spell properly

--- 0x1C-binary_trees/13-binary_tree_nodes.c
+++ 0x1C-binary_trees/13-binary_tree_nodes.c
@@ -7,7 +7,7 @@
  **/
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	if (tree != NULL && !(tree->left == NULL && tree->righ == NULL))
+	if (tree != NULL && !(tree->left == NULL && tree->right == NULL))
 		return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 	return (0);
 }
