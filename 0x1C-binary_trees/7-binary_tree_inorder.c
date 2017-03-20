4c3472bbca605002f5f86ff7e46d1b3030ea488c
corbin-coleman <corbincol@hotmail.com>
2017-03-20 21:33:48 +0000

    Check for function pointer

--- 0x1C-binary_trees/7-binary_tree_inorder.c
+++ 0x1C-binary_trees/7-binary_tree_inorder.c
@@ -1,8 +1,13 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_inorder - Traverse the tree using inorder logic
+ * @tree: The tree to traverse through
+ * @func: The function to call on the nodes
+ **/
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree != NULL)
+	if (tree != NULL && func != NULL)
 	{
 		if (tree->left != NULL)
 			binary_tree_inorder(tree->left, func);
--- 0x1C-binary_trees/8-binary_tree_postorder.c
+++ 0x1C-binary_trees/8-binary_tree_postorder.c
@@ -1,8 +1,13 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_postorder - Traverse tree using postorder logic
+ * @tree: The tree to traverse
+ * @func: Function to call on each node
+ **/
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree != NULL)
+	if (tree != NULL && func != NULL)
 	{
 		if (tree->left != NULL)
 			binary_tree_postorder(tree->left, func);
