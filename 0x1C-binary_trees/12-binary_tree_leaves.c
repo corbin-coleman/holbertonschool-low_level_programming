5e6853b72c52805f7720531f4f9c9ceeea321e54
corbin-coleman <corbincol@hotmail.com>
2017-03-20 21:39:06 +0000

    Don't use other function

--- 0x1C-binary_trees/12-binary_tree_leaves.c
+++ 0x1C-binary_trees/12-binary_tree_leaves.c
@@ -1,10 +1,15 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_leaves - Count the leaves in a tree
+ * @tree: Tree to count leaves in
+ * Return: Number of leaves
+ **/
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	if (!(binary_tree_is_leaf(tree)))
+	if (!(tree->left == NULL && tree->right == NULL))
 		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 	return (1);
 }
