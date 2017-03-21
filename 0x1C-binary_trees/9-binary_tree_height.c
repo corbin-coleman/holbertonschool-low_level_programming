a192c666a96c3ee196bc383419aa3a4ea1f0a41b
corbin-coleman <corbincol@hotmail.com>
2017-03-21 09:17:31 +0000

    Betty

--- 0x1C-binary_trees/9-binary_tree_height.c
+++ 0x1C-binary_trees/9-binary_tree_height.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_height - Find the height of the longest branch in a binary tree
+ * @tree: The tree to find the height of
+ * Return: left_height if left branch is bigger, right_height if right branch
+ * is bigger, 0 if tree is NULL or tree is a leaf
+ **/
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
