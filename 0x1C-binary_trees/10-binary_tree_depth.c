45ec9f03eb6cae3c59db6d9f2fcbec663fc9d124
corbin-coleman <corbincol@hotmail.com>
2017-03-21 09:19:29 +0000

    Betty

--- 0x1C-binary_trees/10-binary_tree_depth.c
+++ 0x1C-binary_trees/10-binary_tree_depth.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_depth - Find the depth of the given node in a binary tree
+ * @node: Node in the binary tree to find depth of
+ * Return: 0 if node is NULL or if node->parent is NULL, 1 + the return of
+ * a recursive call on the node->parent
+ **/
 size_t binary_tree_depth(const binary_tree_t *node)
 {
 	if (node == NULL)
