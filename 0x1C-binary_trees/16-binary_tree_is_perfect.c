74298cea485f7c969dbffc257b20883ca04ffb45
corbin-coleman <corbincol@hotmail.com>
2017-03-21 09:37:59 +0000

    Find height of both branches to prove depth of all leaves is same

--- 0x1C-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1C-binary_trees/16-binary_tree_is_perfect.c
@@ -1,10 +1,43 @@
 #include "binary_trees.h"
-#include "15-binary_tree_is_full.c"
 
-
-int binary_tree_is_perfect(const binary_tree_t *tree)
+/**
+ * find_height - Find height of a binary tree
+ * @tree: Tree to find the height of
+ * Return: left_height if left branch is bigger, right_height if right branch
+ * is bigger, 0 if tree is NULL or tree is a leaf
+ **/
+size_t find_height(const binary_tree_t *tree)
 {
+	size_t left_height, right_height;
+
+	left_height = right_height = 0;
 	if (tree == NULL)
 		return (0);
-	return (binary_tree_is_full(tree));
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+	left_height = find_height(tree->left) + 1;
+	right_height = find_height(tree->right) + 1;
+	if (left_height > right_height)
+		return (left_height);
+	return (right_height);
+}
+
+/**
+ * binary_tree_is_perfect - Check if the given tree is perfect
+ * @tree: Tree to check
+ * Return: 1 if full, 0 if not
+ **/
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+	left_height = find_height(tree->left);
+	right_height = find_height(tree->right);
+	if (left_height != right_height)
+		return (0);
+	if (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right))
+	    return (1);
+	return (1);
 }
