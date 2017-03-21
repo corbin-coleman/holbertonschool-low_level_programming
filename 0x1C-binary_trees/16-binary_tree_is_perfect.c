fd3f666c08ad1c4bef6b823acb2b4a3e6704ae2b
corbin-coleman <corbincol@hotmail.com>
2017-03-21 09:39:17 +0000

    Betty

--- 0x1C-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1C-binary_trees/16-binary_tree_is_perfect.c
@@ -38,6 +38,6 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (left_height != right_height)
 		return (0);
 	if (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right))
-	    return (1);
+		return (1);
 	return (1);
 }
