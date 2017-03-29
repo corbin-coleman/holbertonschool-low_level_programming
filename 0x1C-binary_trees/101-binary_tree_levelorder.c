bda86576fd48a7dfdf19350d0a43185598a0601d
corbin-coleman <corbincol@hotmail.com>
2017-03-29 05:31:51 +0000

    Task 20 - Level order traversal of a tree

---
+++ 0x1C-binary_trees/101-binary_tree_levelorder.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_levelorder - Traverse a tree in level order, starts with root,
+ * then left child, then right child
+ * @tree: The tree to traverse
+ * @func: Function to call on the tree
+ **/
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL)
+		return;
+	if (tree->parent == NULL)
+		func(tree->n);
+	if (tree->left != NULL)
+		func(tree->left->n);
+	if (tree->right != NULL)
+		func(tree->right->n);
+	binary_tree_levelorder(tree->left, func);
+	binary_tree_levelorder(tree->right, func);
+}
