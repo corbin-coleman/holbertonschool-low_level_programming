a9fb56f55bae46942daf2387b4ddd44a981d8e17
corbin-coleman <corbincol@hotmail.com>
2017-03-29 06:54:47 +0000

    Task 22 - Rotate a binary tree to the left

---
+++ 0x1C-binary_trees/103-binary_tree_rotate_left.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_rotate_left - Rotate the given node to the left
+ * @tree: The given node to tree to rotate
+ * Return: The new root of the tree, NULL if it fails
+ **/
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (NULL);
+	tree->parent = tree->right;
+	tree->right = tree->parent->left;
+	tree->parent->left = tree;
+	tree->parent->parent = NULL;
+	return (tree->parent);
+}
