d0773161b20c23b0c9f92d9b87b799abfd696f96
corbin-coleman <corbincol@hotmail.com>
2017-03-29 05:37:38 +0000

    Check for NULL function pointer

--- 0x1C-binary_trees/101-binary_tree_levelorder.c
+++ 0x1C-binary_trees/101-binary_tree_levelorder.c
@@ -8,7 +8,7 @@
  **/
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL)
+	if (tree == NULL || func == NULL)
 		return;
 	if (tree->parent == NULL)
 		func(tree->n);
