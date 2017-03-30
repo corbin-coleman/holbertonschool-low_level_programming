21a653afa66321f4deee230bd12cc03067c1bc98
corbin-coleman <corbincol@hotmail.com>
2017-03-30 01:07:21 +0000

    Update header

--- 0x1D-search_algorithms/search_algos.h
+++ 0x1D-search_algorithms/search_algos.h
@@ -3,6 +3,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Mandatory Functions */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+
+/* Advanced Functions */
+int jump_search(int *array, size_t size, int value);
 #endif
