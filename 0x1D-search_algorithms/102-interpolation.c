c8fc405f753b6404ccd58fdcb37e277fafd29ab0
corbin-coleman <corbincol@hotmail.com>
2017-03-30 03:24:45 +0000

    Betty

---
+++ 0x1D-search_algorithms/102-interpolation.c
@@ -0,0 +1,40 @@
+#include "search_algos.h"
+
+/**
+ * interpolation_search - Interpolation search algorithm
+ * @array: Array to search
+ * @size: Size of the array
+ * @value: Value to search for
+ * Return: The index of the value in the array, -1 if not found
+ **/
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t low, high, pos;
+
+	low = 0;
+	high = size - 1;
+
+	while (array[high] != array[low])
+	{
+		pos = low + (((double)(high - low) /
+			      (array[high] - array[low]))
+			     * (value - array[low]));
+
+		if (pos > size)
+		{
+			printf("Value checked array[%lu] is out of range\n", pos);
+			return (-1);
+		}
+		else
+			printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+		if (array[pos] < value)
+			low = pos + 1;
+		else if (array[pos] > value)
+			high = pos - 1;
+		else
+			return (pos);
+	}
+	if (array[low] == value)
+		return (low);
+	return (-1);
+}
