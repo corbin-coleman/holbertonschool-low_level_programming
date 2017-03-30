018da7fcc58a3ee50fa20168e3fd6a72fdd41821
corbin-coleman <corbincol@hotmail.com>
2017-03-30 02:59:27 +0000

    Update header file for Tasks 7 & 9

--- 0x1D-search_algorithms/search_algos.h
+++ 0x1D-search_algorithms/search_algos.h
@@ -2,6 +2,7 @@
 #define SEARCH_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 /* Mandatory Functions */
 int linear_search(int *array, size_t size, int value);
@@ -9,4 +10,5 @@ int binary_search(int *array, size_t size, int value);
 
 /* Advanced Functions */
 int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
 #endif
