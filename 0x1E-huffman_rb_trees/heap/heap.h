644ea2e915e701778249d6ef2acdbf2c72a16848
corbin-coleman <corbincol@hotmail.com>
2017-05-17 19:05:48 +0000

    Update with heap_extract function

--- 0x1E-huffman_rb_trees/heap/heap.h
+++ 0x1E-huffman_rb_trees/heap/heap.h
@@ -36,4 +36,5 @@ heap_t *heap_create(int (*data_cmp)(void *, void *));
 binary_tree_node_t *binary_tree_node(binary_tree_node_t *parent, void *data);
 binary_tree_node_t *heap_insert(heap_t *heap, void *data);
 void heap_delete(heap_t *heap, void (*free_data)(void *));
+void *heap_extract(heap_t *heap);
 #endif
