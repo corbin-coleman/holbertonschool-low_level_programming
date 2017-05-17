b7272b9de5bd6f9f46dcb62ad391138699d8b217
corbin-coleman <corbincol@hotmail.com>
2017-05-17 18:52:24 +0000

    Updated with heap_delete function

--- 0x1E-huffman_rb_trees/heap/heap.h
+++ 0x1E-huffman_rb_trees/heap/heap.h
@@ -35,4 +35,5 @@ typedef struct heap_s
 heap_t *heap_create(int (*data_cmp)(void *, void *));
 binary_tree_node_t *binary_tree_node(binary_tree_node_t *parent, void *data);
 binary_tree_node_t *heap_insert(heap_t *heap, void *data);
+void heap_delete(heap_t *heap, void (*free_data)(void *));
 #endif
