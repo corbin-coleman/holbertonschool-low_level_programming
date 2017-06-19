7a6098968d43e54952a52f3d78c1cdb5d13e7f69
Corbin Coleman <corbincol@hotmail.com>
2017-06-19 16:48:08 -0700

    Add win struct for win square coordinates and win_value to track if player has won. Add usleep function so player doesn't spin/move too fast

--- maze/main_maze.c
+++ maze/main_maze.c
@@ -10,6 +10,8 @@ int main(int argc, char *argv[])
 {
 	SDL_Instance instance;
 	char **map;
+	int win_value = 0;
+	int_s win = {0, 0};
 	double_s play = {2, 2};
 	double_s dir = {-1, 0};
 	double_s plane = {0, 0.5};
@@ -17,7 +19,7 @@ int main(int argc, char *argv[])
 
 	if (argc < 2)
 		return (1);
-	map = create_map(argv[1], &play);
+	map = create_map(argv[1], &play, &win);
 	if (map == NULL)
 	{
 		printf("Unable to create map\n");
@@ -30,19 +32,18 @@ int main(int argc, char *argv[])
 	}
 	while (1)
 	{
-/*		key_press.up = 0;
-		key_press.down = 0;
-		key_press.left = 0;
-		key_press.right = 0;*/
 		if (keyboard_events(&key_press))
 			break;
 		movement(key_press, &plane, &dir, &play, map);
-		SDL_SetRenderDrawColor(instance.renderer, 0, 0, 0, 0);
-		SDL_RenderClear(instance.renderer);
+		if (check_win(play, win, &win_value))
+			break;
 		draw_background(instance);
 		draw_walls(map, play, instance, dir, plane);
 		SDL_RenderPresent(instance.renderer);
+		usleep(150 * 1000);
 	}
+	if (win_value)
+		print_win();
 	SDL_DestroyRenderer(instance.renderer);
 	SDL_DestroyWindow(instance.window);
 	SDL_Quit();
