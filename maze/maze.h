b36e4c6791ba1ef60acbb7a7ec5bef0cfad8d0b5
Corbin Coleman <corbincol@hotmail.com>
2017-06-19 17:04:10 -0700

    Add all the new functions and update old functions to handle new win condition

--- maze/maze.h
+++ maze/maze.h
@@ -48,6 +48,13 @@ typedef struct int_s
 	int y;
 } int_s;
 
+/**
+ * struct keys - Struct to keep track of key presses
+ * @up: Is up pressed (1) or not (0)
+ * @down: Is down pressed (1) or not (0)
+ * @right: Is right pressed (1) or not (0)
+ * @left: Is left pressed (1) or not (0)
+ **/
 typedef struct keys
 {
 	int up;
@@ -59,10 +66,17 @@ typedef struct keys
 int init_instance(SDL_Instance *);
 void fill_screen_by_line(SDL_Instance instance);
 int keyboard_events(keys *key_press);
-char **create_map(char *file_string, double_s *play);
+char **create_map(char *file_string, double_s *play, int_s *win);
 void draw_walls(char **map, double_s play, SDL_Instance, double_s, double_s);
 void choose_color(SDL_Instance, char **map, int_s coord, int hit_side);
 void rotate(double_s *plane, double_s *dir, int rot_dir);
-void movement(keys key_press, double_s *plane, double_s *dir, double_s *play, char **map);
+void movement(keys key_press, double_s *plane, double_s *dir, double_s *play,
+	      char **map);
 void draw_background(SDL_Instance);
+void print_win(void);
+int check_win(double_s play, int_s win, int *win_value);
+void plot_grid_points(char **maze, double_s *play, int_s *win, size_t cur_char,
+		      size_t maze_line, char *line);
+void check_key_release_events(SDL_Event event, keys *key_press);
+int check_key_press_events(SDL_Event event, keys *key_press);
 #endif
