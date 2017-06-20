ab2ee557906d857bc12800ae58e7ca7e6cdcd2ab
Corbin Coleman <corbincol@hotmail.com>
2017-06-19 17:08:25 -0700

    Remove variable names from prototypes of functions in header file to minimize character count for lines and be more consistent in prototypes (some alread had no variable names)

--- maze/maze.h
+++ maze/maze.h
@@ -64,19 +64,17 @@ typedef struct keys
 } keys;
 
 int init_instance(SDL_Instance *);
-void fill_screen_by_line(SDL_Instance instance);
-int keyboard_events(keys *key_press);
-char **create_map(char *file_string, double_s *play, int_s *win);
-void draw_walls(char **map, double_s play, SDL_Instance, double_s, double_s);
-void choose_color(SDL_Instance, char **map, int_s coord, int hit_side);
-void rotate(double_s *plane, double_s *dir, int rot_dir);
-void movement(keys key_press, double_s *plane, double_s *dir, double_s *play,
-	      char **map);
+void fill_screen_by_line(SDL_Instance);
+int keyboard_events(keys *);
+char **create_map(char *, double_s *, int_s *);
+void draw_walls(char **, double_s, SDL_Instance, double_s, double_s);
+void choose_color(SDL_Instance, char **, int_s, int);
+void rotate(double_s *, double_s *, int);
+void movement(keys, double_s *, double_s *, double_s *, char **);
 void draw_background(SDL_Instance);
 void print_win(void);
-int check_win(double_s play, int_s win, int *win_value);
-void plot_grid_points(char **maze, double_s *play, int_s *win, size_t cur_char,
-		      size_t maze_line, char *line);
-void check_key_release_events(SDL_Event event, keys *key_press);
-int check_key_press_events(SDL_Event event, keys *key_press);
+int check_win(double_s, int_s, int *);
+void plot_grid_points(char **, double_s *, int_s *, size_t, size_t, char *);
+void check_key_release_events(SDL_Event, keys *);
+int check_key_press_events(SDL_Event, keys *);
 #endif
